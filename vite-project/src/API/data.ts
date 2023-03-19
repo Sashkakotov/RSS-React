7dffadd3bd9920c4b7657aa79ce188698c35f207
Sashkakotov <sashkakotov@mail.ru>
2023-03-19 15:59:04 +0300

    fix: image url

--- vite-project/src/API/data.ts
+++ vite-project/src/API/data.ts
@@ -5,7 +5,7 @@ const catsData = {
       name: 'Eva',
       breed: 'Devon Rex',
       description: 'This is my cat, she hates all people except me and my wife',
-      photo: 'public/eva.jpg',
+      photo: './eva.jpg',
     },
     {
       id: '2',
@@ -13,7 +13,7 @@ const catsData = {
       breed: 'Sphinx',
       description:
         'Loki is the new most severe and gloomy cat in the world, who fell in love with the Internet.',
-      photo: 'public/loki.jpg',
+      photo: './loki.jpg',
     },
 
     {
@@ -22,14 +22,14 @@ const catsData = {
       breed: 'Snowshoe',
       description:
         "Grumpy Cat, real name is Tardar Sauce. A cat who became an internet celebrity for her unusual appearance after her owner's brother posted a picture of the cat on Reddit in September 2012.",
-      photo: 'public/snow-shu.jpg',
+      photo: './snow-shu.jpg',
     },
     {
       id: '4',
       name: 'Kouky',
       breed: 'Scottish Fold',
       description: 'Koyuki is a Japanese angry cat who looks like the true embodiment of evil.',
-      photo: 'public/kouky.jpg',
+      photo: './kouky.jpg',
     },
 
     {
@@ -38,7 +38,7 @@ const catsData = {
       breed: 'Himalayan',
       description:
         'The Guinness Book of Records has officially recorded that Colonel Meow with a coat length of 23 cm is the longest-haired cat in the world.',
-      photo: 'public/polovnik-miu.jpg',
+      photo: './polovnik-miu.jpg',
     },
     {
       id: '6',
@@ -46,14 +46,14 @@ const catsData = {
       breed: 'Persian cat',
       description:
         "If Grumpy Cat is an angry cat, then Persian cat Garfi is rather angry. Or he's obviously plotting to kill you.",
-      photo: 'public/Garfi.jpg',
+      photo: './Garfi.jpg',
     },
     {
       id: '7',
       name: 'Albert',
       breed: 'Selkirk rex',
       description: 'Curly-haired evil: cat Albert, which is very similar to the evil lamb',
-      photo: 'public/Albert.jpg',
+      photo: './Albert.jpg',
     },
   ],
 };
