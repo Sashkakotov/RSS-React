d7024c47fa58f122613f9f4209b26299e9cfc94d
Sashkakotov <sashkakotov@mail.ru>
2023-03-31 16:39:37 +0300

    feat: localStorage

--- vite-project/src/components/UI/SearchInput/SearchInput.tsx
+++ vite-project/src/components/UI/SearchInput/SearchInput.tsx
@@ -10,30 +10,31 @@ import React, {
 } from 'react';
 
 const SearchInput = (props) => {
-  // const [searchInputValue, setSearchInputValue] = useState();
+  const [searchInputValue, setSearchInputValue] = useState(
+    localStorage.getItem('searchValue') || ''
+  );
 
-  // const searchInputRef = useRef<string>();
+  const searchInputRef = useRef<string>();
 
-  // useEffect(() => {
-  //   searchInputRef.current = searchInputValue;
-  // });
-  // useEffect(() => {
-  //   return () => {
-  //     localStorage.setItem('searchValue', String(searchInputRef.current));
-  //   };
-  // });
-
-  //
-
+  useEffect(() => {
+    searchInputRef.current = searchInputValue;
+  });
+  useEffect(() => {
+    return () => {
+      localStorage.setItem('searchValue', String(searchInputRef.current));
+    };
+  });
+  const handleChanges = (e: React.SyntheticEvent) =>
+    setSearchInputValue((e.target as HTMLInputElement).value);
   return (
     <div className="search-input__container">
       <input
-        // onChange={handleChange}
+        onChange={handleChanges}
         onKeyDown={props.change}
         type="text"
         className="search__input"
         placeholder={SEARCH_INPUT_PLACEHOLDER}
-        // value={searchInputValue}
+        value={searchInputValue}
         data-testid="search-input"
       />
     </div>
--- vite-project/src/pages/Home.tsx
+++ vite-project/src/pages/Home.tsx
@@ -11,17 +11,21 @@ const Home = () => {
   const [cats, setCats] = useState([] as flickrResponse[]);
   const [loader, setLoader] = useState(false);
 
-  // useEffect(() => {
-  //   const result = getCats();
-  //   if (result) {
-  //     setCats(result);
-  //   }
-  // }, []);
+  useEffect(() => {
+    const fetchingData = async () => {
+      const ls = localStorage.getItem('searchValue');
+      if (ls) {
+        const response = await getFlickrCards(ls);
+        setCats(await response);
+      }
+    };
+    fetchingData();
+  }, []);
 
   const getFlickrCards = async (search: string) => {
     setLoader(true);
     try {
-      const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e1ef2eb52dead24effea7dc5ec0fd405&text=${search}&tags=cats&per_page=&page=&format=json&nojsoncallback=1`;
+      const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=e1ef2eb52dead24effea7dc5ec0fd405&text=${search}&tags=cats&per_page=10&page=&format=json&nojsoncallback=1`;
       const response = await fetch(url);
       const result = await response.json();
       return result.photos.photo;
